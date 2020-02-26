fd4e9fbb380524d26a55d8a9a5585e2d9e5d903f
Alkis Kordas <sdi1400249@di.uoa.gr>
2020-02-26 20:31:35 +0200

    Added the file that actually handles the dice requests.

---
+++ pkg/server/dice_server.go
@@ -0,0 +1,187 @@
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/aakordas/creature_manager/pkg/dice"
+	"github.com/gorilla/mux"
+)
+
+type rollResponse struct {
+	Count  int `json:"count"`  // The number of dice that got rolled.
+	Sides  int `json:"sides"`  // The number of sides each dice had.
+	Result int `json:"result"` // The result of the rolling.
+}
+
+type rollErrorResponse struct {
+	Error        string `json:"error"`
+	ErrorMessage string `json:"error_message"`
+}
+
+// writeHeader writes the header of a valid response.
+func writeHeader(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+}
+
+// jsonEncode wraps the json.Encoder.Encode and error checking.
+func jsonEncode(w http.ResponseWriter, enc *json.Encoder, v interface{}) {
+	if err := enc.Encode(v); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// rollDice rolls the specified number of the specified dice.
+func rollDice(sides, count int) (result int) {
+	d := chooseDice(sides)
+	if d == nil {
+		return 0
+	}
+
+	for i := 0; i < count; i++ {
+		result += d()
+	}
+
+	return result
+}
+
+// getSides gets the integer value from the sides from the passed string.
+func getSides(sides string) int {
+	if sides == "" {
+		return 20
+	}
+
+	s, err := strconv.Atoi(sides)
+	if err != nil {
+		return 0
+	}
+
+	return s
+}
+
+// getCount gets the integer value from the count from the passed string.
+func getCount(count string) int {
+	if count == "" {
+		return 1
+	}
+
+	c, err := strconv.Atoi(count)
+	if err != nil {
+		return 0
+	}
+
+	return c
+}
+
+func unexpectedError(w http.ResponseWriter) {
+	http.Error(
+		w,
+		"The server might have encountered an error. Please try again.",
+		http.StatusInternalServerError,
+	)
+}
+
+// Roll is the handler for all the requested rolls of one die.
+func Roll(w http.ResponseWriter, r *http.Request) {
+	sides := r.FormValue("sides")
+	count := r.FormValue("count")
+
+	s := getSides(sides)
+	if s == 0 {
+		unexpectedError(w)
+		return
+	}
+
+	c := getCount(count)
+	if c == 0 {
+		unexpectedError(w)
+		return
+	}
+
+	response(w, s, c)
+}
+
+// chooseDice chooses the appropriate dice given a number of sides and returns
+// it and the number of sides as an integer..
+func chooseDice(sides int) dice.Dice {
+	switch sides {
+	case 4:
+		return dice.D4
+	case 6:
+		return dice.D6
+	case 8:
+		return dice.D8
+	case 10:
+		return dice.D10
+	case 12:
+		return dice.D12
+	case 20:
+		return dice.D20
+	case 100:
+		return dice.D100
+	default:
+		return nil
+	}
+}
+
+// response deals with the response part of the HTTP response, whether that is an error response or not.
+func response(w http.ResponseWriter, s, c int) {
+	writeHeader(w)
+
+	result := rollDice(s, c)
+	enc := json.NewEncoder(w)
+	if result == 0 {
+		errorResponse := rollErrorResponse{"invalid sides", "The dice requested is not available."}
+		jsonEncode(w, enc, errorResponse)
+		return
+	}
+
+	response := rollResponse{c, s, result}
+	jsonEncode(w, enc, response)
+}
+
+// RollN is the handler for all the requested rolls of n d dice, where n is
+// specified with a query.
+func RollN(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	sides := vars["sides"]
+	count := r.FormValue("count")
+
+	s := getSides(sides[1:])
+	if s == 0 {
+		unexpectedError(w)
+		return
+	}
+
+	c := getCount(count)
+	if c == 0 {
+		unexpectedError(w)
+		return
+	}
+
+	response(w, s, c)
+}
+
+// DRollN is the handlre for all the requested rolls of n d dice, where n is
+// specified with a variable.
+func DRollN(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	sides := vars["sides"]
+	count := vars["count"]
+
+	s := getSides(sides[1:])
+	if s == 0 {
+		unexpectedError(w)
+		return
+	}
+
+	c := getCount(count)
+	if c == 0 {
+		unexpectedError(w)
+		return
+	}
+
+	response(w, s, c)
+}
