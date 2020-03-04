66250395d8f69beaced07b384cb980e2a5854c45
Alkis Kordas <sdi1400249@di.uoa.gr>
2020-03-04 23:12:12 +0200

    Added level attribute to Creature and proficiency bonus per level.

--- pkg/creature/creature.go
+++ pkg/creature/creature.go
@@ -12,6 +12,7 @@ type Creature struct {
 	Name string `json:"name" bson:"name"`
 
 	CurrentHitPoints int `json:"hit_points" bson:"hit_points"`
+	Level            int `json:"level" bson:"level"`
 
 	abilities.Abilities `json:"abilities" bson:"abilities"`
 	skills.Skills       `json:"skills,omitempty" bson:"skills,omitempty"`
@@ -21,4 +22,28 @@ type Creature struct {
 	ArmorClass       int `json:"armor_class" bson:"armor_class"`
 
 	PassivePerception int `json:"passive_perception" bson:"passive_perception"` // FIXME: Include any other bonuses.
+}
+
+// ProficiencyBonusPerLevel maps a character level to the proficiency bonus.
+var ProficiencyBonusPerLevel = map[int]int{
+	1:  2,
+	2:  2,
+	3:  2,
+	4:  2,
+	5:  3,
+	6:  3,
+	7:  3,
+	8:  3,
+	9:  4,
+	10: 4,
+	11: 4,
+	12: 4,
+	13: 5,
+	14: 5,
+	15: 5,
+	16: 5,
+	17: 6,
+	18: 6,
+	19: 6,
+	20: 6,
 }
