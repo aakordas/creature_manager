5498a74e3ec9523c9bb55f88b293872d44a773ec
Alkis Kordas <aakordas96@gmail.com>
2020-03-26 21:52:39 +0200

    Added proper testing with gofight for the Roll handler.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/aakordas/creature_manager
 go 1.14
 
 require (
+	github.com/appleboy/gofight/v2 v2.1.2
 	github.com/gorilla/mux v1.7.4
 	go.mongodb.org/mongo-driver v1.3.0
 )
--- pkg/server/dice_server_test.go
+++ pkg/server/dice_server_test.go
@@ -1,59 +1,62 @@
 package server
 
 import (
+	"bytes"
+	"net/http"
+	"os"
 	"testing"
 
-	"github.com/aakordas/creature_manager/pkg/dice"
+	"github.com/appleboy/gofight/v2"
+	"github.com/gorilla/mux"
 )
 
-func Test_getSides(t *testing.T) {
-	type args struct {
-		sides string
+var router *mux.Router
+
+func diceRouter() {
+	router = mux.NewRouter()
+	router = diceRoutes(router)
+	router = playerRoutes(router)
+}
+
+// TestRoll tests the Roll handler.
+func TestRoll(t *testing.T) {
+	type response struct {
+		Code int
+		Body string
 	}
 	tests := []struct {
 		name string
-		args args
-		want int
+		args string
+		want response
 	}{
-		{"No sides", args{""}, 20},
-		{"Valid sides", args{"4"}, 4},
-		{"Valid sides, invalid dice", args{"1"}, 1},
-		{"Invalid sides", args{"a"}, 0},
+		{"Default roll", "", response{http.StatusOK, ``}},
+		{"Valid query for sides", "?sides=4", response{http.StatusOK, `"sides":4`}},
+		{"Invalid query for sides", "?sides=5", response{http.StatusNotAcceptable, `"invalid sides"`}},
+		{"Valid query for count", "?count=2", response{http.StatusOK, `"count":2`}},
+		{"Invalid query for count", "?count=0", response{http.StatusNotAcceptable, `"invalid count"`}},
+		{"Valid query for sides, invalid for count", "?sides=4&count=0", response{http.StatusNotAcceptable, `"invalid count"`}},
+		{"Valid query for count, invalid for sides", "?count=2&sides=1", response{http.StatusNotAcceptable, `"invalid sides"`}},
+		{"Valid query for sides and count", "?sides=4&count=2", response{http.StatusOK, `"count":2,"sides":4`}},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := getSides(tt.args.sides); got != tt.want {
-				t.Errorf("getSides() = %v, want %v", got, tt.want)
-			}
+			r := gofight.New()
+
+			r.GET("/api/v1/roll"+tt.args).
+				Run(router, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
+					if r.Code != tt.want.Code {
+						t.Errorf("Handler returned wrong status code: got %v want %v", r.Code, tt.want.Code)
+					}
+
+					if !bytes.Contains(r.Body.Bytes(), []byte(tt.want.Body)) {
+						t.Errorf("Unexpected body returned.\ngot %v\nwant %v", r.Body, tt.want.Body)
+					}
+				})
 		})
 	}
 }
 
-func Test_getCount(t *testing.T) {
-	type args struct {
-		count string
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		{"No count", args{""}, 1},
-		{"Valid count", args{"2"}, 2},
-		{"Invalid count", args{"a"}, 0},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := getCount(tt.args.count); got != tt.want {
-				t.Errorf("getCount() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_chooseDice(t *testing.T) {
-	type args struct {
-		sides int
 	}
 	tests := []struct {
 		name string
@@ -70,4 +73,17 @@ func Test_chooseDice(t *testing.T) {
 			}
 		})
 	}
+}
+
+// TestMain TODO: NEEDS COMMENT INFO
+func TestMain(m *testing.M) {
+	var (
+		serverPort    = ":8080"
+		serverAddress = "http://127.0.0.1" + serverPort
+	)
+	_ = serverAddress
+
+	diceRouter()
+
+	os.Exit(m.Run())
 }
